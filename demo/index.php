8e7dd73196d9ff12e487dac5f71ebd1f88006da2
Grégoire Pineau <lyrixx@lyrixx.info>
2012-10-23 00:05:08 +0200

    Fixed demo

--- demo/index.php
+++ demo/index.php
@@ -5,90 +5,57 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 // Add a small Dependency Injector
 $c = new Pimple();
 
-// Load DI default values
-require_once(__DIR__ . '/di.php');
+// Load DIC default values
 
-// Define some username
-$c['my_lifestream.twitter'] = $c['lifestream.factory'](new Lifestream\Service\Twitter($c['browser'], 'lyrixx'), 'twitter.lyrixx');
-// $c['twitter.username']      = 'lyrixx';
-// $c['delicious.username']    = 'lyrixx86';
-// $c['rss.feed']              = 'http://feeds2.feedburner.com/lyrixblog';
-// $c['lastfm.username']       = 'lyrix86';
-// $c['github.username']       = 'lyrixx';
-// $c['flickr.username']       = 'xavierbriand';
-// $c['flickr.userid']         = '34871318';
+$c['browser'] = $c->share(function () {
+    return new Buzz\Browser(new Buzz\Client\Curl());
+});
 
-// // Define Some filters
-// $c['filters.twitter'] = function($c){
-//     return new Lifestream\Model\Filter\Twitter();
-// };
-// // Define some twitter filters
-// $c['twitter.filters'] = function($c){
-//     return array($c['filters.twitter']);
+$c['lifestream.factory'] = $c->protect(function($service, $id) use($c) {
+    $lifestream = new Lifestream\Lifestream($service);
 
-// };
-
-// // Define Some formatters
-// $c['formatter.link'] = function($c){
-//     return new Lifestream\Model\Formatter\Link();
-// };
-// // Define Some twitter formatters
-// $c['twitter.formatters'] = function($c){
-//     return array($c['formatter.link']);
-// };
-
-// Define default displayer
-function displayStatus($status)
-{
-    echo $status->getText();
-}
-
-function displayStream($stream)
-{
-    echo '<ul>';
-    foreach ($stream as $status) {
-        echo '<li>';
-        displayStatus($status);
-        echo '</li>';
+    if (isset($c['my_lifestream.'.$id.'.filters'])) {
+        $lifestream->setFilters($c['my_lifestream'.$id.'.filters']);
     }
+    if (isset($c['my_lifestream.'.$id.'.formatters'])) {
+        $lifestream->setFormatters($c['my_lifestream'.$id.'.formatters']);
+    }
+
+    return $c['my_lifestream.'.$id] = $lifestream;
+
+});
+
+$c['lifestream.display'] = $c->protect(function($lifestream, $title = null) {
+    echo '<h2>';
+        echo '<a href="' . $lifestream->getService()->getProfileUrl() . '">';
+            echo $title ?: $lifestream->getService()->getName();
+        echo '</a>';
+    echo '</h2>';
+    echo '<ul>';
+        foreach ($lifestream->boot()->getStream() as $status) {
+            echo '<li>';
+                echo $status;
+            echo '</li>';
+        }
     echo '</ul>';
-}
-?>
+    echo '<hr>';
+});
 
-<h1>LifeStream</h1>
-<?php
 
-$twitter = $c['my_lifestream.twitter'];
-echo '<h2><a href="' . $twitter->getService()->getProfileUrl() . '">Twitter</a></h2>';
-displayStream($twitter->boot()->getStream(10));
-echo '<hr>';
+// Config:
+$lsf = $c['lifestream.factory'];
 
-// $twitter->setUsername('fabpot');
-// echo '<h2><a href="' . $twitter->getServiceURL() . '">Twitter</a></h2>';
-// displayStream($twitter->processFeed()->getStream(10));
-// echo '<hr>';
+$lsf(new Lifestream\Service\Twitter($c['browser'], 'lyrixx'), 'twitter.lyrixx');
+$lsf(new Lifestream\Service\Github($c['browser'], 'lyrixx'),  'github.lyrixx');
+$lsf(new Lifestream\Service\Rss20($c['browser'], 'http://feeds2.feedburner.com/lyrixblog'), 'rss.lyrixx');
+$lsf(new Lifestream\Service\FlickrRss20($c['browser'], '34871318', 'xavierbriand'), 'flicker.xavierbriand');
 
-// $delicious = $c['delicious'];
-// echo '<h2><a href="'.$delicious->getServiceURL().'">Delicious</a></h2>';
-// displayStream($delicious->processFeed()->getStream(10));
-// echo '<hr>';
+// $c['delicious.username']    = 'lyrixx86';
+// $c['lastfm.username']       = 'lyrix86';
 
-// $rss = $c['rss'];
-// echo '<h2><a href="'.$rss->getServiceURL().'">RSS</a></h2>';
-// displayStream($rss->processFeed()->getStream(10));
-// echo '<hr>';
+echo '<h1>LifeStream</h1>';
 
-// $lastfm = $c['lastfm'];
-// echo '<h2><a href="' . $lastfm->getServiceURL() . '">LastFM</a></h2>';
-// displayStream($lastfm->processFeed()->getStream(10));
-// echo '<hr>';
-
-// $github = $c['github'];
-// echo '<h2><a href="'.$github->getServiceURL().'">Github</a></h2>';
-// displayStream($github->processFeed()->getStream(10));
-// echo '<hr>';
-
-// $flickr = $c['flickr'];
-// echo '<h2><a href="'.$flickr->getServiceURL().'">Flickr</a></h2>';
-// displayStream($flickr->processFeed()->getStream(10));
-// echo '<hr>';
+$c['lifestream.display']($c['my_lifestream.twitter.lyrixx']);
+$c['lifestream.display']($c['my_lifestream.github.lyrixx']);
+$c['lifestream.display']($c['my_lifestream.rss.lyrixx']);
+$c['lifestream.display']($c['my_lifestream.flicker.xavierbriand']);
