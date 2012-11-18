1d7a429e51c90ab3248a0b92e8111a3c1dc4b807
Grégoire Pineau <lyrixx@lyrixx.info>
2012-11-18 15:00:04 +0100

    Added filter retweet on TwitterFilter
    + Added a way to configure which filters should be callec

--- Lifestream/Tests/Filter/TwitterTest.php
+++ Lifestream/Tests/Filter/TwitterTest.php
@@ -11,6 +11,33 @@ class TwitterTest extends \PHPUnit_Framework_TestCase
         $this->twitter = new Twitter();
     }
 
+    public function testConfiguration()
+    {
+        $twitter = new Twitter(0);
+        $this->assertFalse($twitter->shouldFilterReply());
+        $this->assertFalse($twitter->shouldFilterRetweet());
+
+        $twitter->enableFilterReply();
+        $this->assertTrue($twitter->shouldFilterReply());
+        $this->assertFalse($twitter->shouldFilterRetweet());
+
+        $twitter->enableFilterRetweet();
+        $this->assertTrue($twitter->shouldFilterReply());
+        $this->assertTrue($twitter->shouldFilterRetweet());
+
+        $twitter->disabledFilterReply();
+        $this->assertFalse($twitter->shouldFilterReply());
+        $this->assertTrue($twitter->shouldFilterRetweet());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testConvertStringFlag()
+    {
+        $this->twitter->enableFilter('foo');
+    }
+
     public function getInvalidateMentionTests()
     {
         return array(
@@ -26,6 +53,32 @@ class TwitterTest extends \PHPUnit_Framework_TestCase
      * @dataProvider getInvalidateMentionTests
      */
     public function testInvalidateMention($expected, $text)
+    {
+        $status = $this->getMock('Lifestream\StatusInterface');
+        $status
+            ->expects($this->any())
+            ->method('getText')
+            ->will($this->returnValue($text))
+        ;
+
+        $this->assertSame($expected, $this->twitter->isValid($status));
+    }
+
+    public function getInvalidateRetweetTests()
+    {
+        return array(
+            array(true, ''),
+            array(true, 'Hi'),
+            array(true, 'Hi @lyrixx RT @lyrixx'),
+            array(false, 'RT @lyrixx'),
+            array(false, 'RT @lyrixx Hi'),
+        );
+    }
+
+    /**
+     * @dataProvider getInvalidateRetweetTests
+     */
+    public function testInvalidateRetweet($expected, $text)
     {
         $status = $this->getMock('Lifestream\StatusInterface');
         $status
