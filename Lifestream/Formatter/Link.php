dcf398a9381a2c124712e4dcaead0a1e044a12fa
Grégoire Pineau <lyrixx@lyrixx.info>
2013-02-24 19:31:38 +0100

    Refactored Formatter/Link

--- Lifestream/Formatter/Link.php
+++ Lifestream/Formatter/Link.php
@@ -7,24 +7,18 @@ use Lyrixx\Lifestream\StatusInterface;
 /**
  * Link formatter transforms all text/plain link
  * into a text/html link
- *
- * @todo Support mailto formatter
- * @todo Support formatter configuration
  */
 class Link implements FormatterInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public function format(StatusInterface $status)
     {
-        return $this->formatHttpLink($status);
-    }
-
-    private function formatHttpLink(StatusInterface $status)
-    {
         return $status->setText(preg_replace(
-            "/(https?:\/\/([^\s]+))/",
-            "<a href=\"$1\">$2</a>",
+            '/(https?:\/\/([^\s]+))/',
+            '<a href="$1">$2</a>',
             $status->getText()
         ));
     }
-
 }
