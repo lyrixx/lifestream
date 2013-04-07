8f522b69d98e710c7406abefdec256ba2968d37e
Grégoire Pineau <lyrixx@lyrixx.info>
2013-04-07 20:35:10 +0200

    Added TwitterList

---
+++ Lifestream/Service/TwitterList.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lyrixx\Lifestream\Service;
+
+/**
+ * Fetch twitter list
+ */
+class TwitterList extends AbstractFeed
+{
+    const FEED_URL    = 'https://api.twitter.com/1/lists/statuses.json?owner_screen_name=%s&slug=%s&include_entities=true';
+    const PROFILE_URL = 'https://twitter.com/%s/%s';
+    const TWEET_URL   = 'https://twitter.com/%s/status/%s';
+
+    /**
+     * Constructor
+     *
+     * @param string $username The twitter username
+     * @param string $list     The twitter list name
+     * @param [type] $client   The client
+     */
+    public function __construct($username, $list, $client = null)
+    {
+        $feedUrl = sprintf(self::FEED_URL, $username, $list);
+        $profileUrl = sprintf(self::PROFILE_URL, $username, $list);
+
+        parent::__construct($feedUrl, $profileUrl, $client);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function extractDatas($datasTmp)
+    {
+        $datas = array();
+        foreach (json_decode((string) $datasTmp, true) as $value) {
+            $datas[] = $this->formatDatas($value);
+        }
+
+        return $datas;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function formatDatas($datas)
+    {
+        return array(
+            'text' => $datas['text'],
+            'url'  => sprintf(static::TWEET_URL, $datas['user']['screen_name'], $datas['id_str']),
+            'date' => new \Datetime($datas['created_at']),
+        );
+    }
+}
---
+++ Lifestream/Tests/Service/TwitterListTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lyrixx\Lifestream\Tests\Service;
+
+use Lyrixx\Lifestream\Service\TwitterList;
+
+class TwitterListTest extends AbstractTest
+{
+    public function testGetStatuses()
+    {
+        $service = new TwitterList('futurecat', 'sensio');
+        $service->setClient($this->getClient(__DIR__.'/Fixtures/TwitterList.json'));
+
+        $statuses = $service->getStatuses();
+        $this->assertCount(12, $statuses);
+
+        $firstStatus = $statuses[0];
+        $this->assertEquals('@lyrixx un autre jour ;)', $firstStatus->getText());
+        $this->assertEquals('https://twitter.com/futurecat/status/320926532933672961', $firstStatus->getUrl());
+        $this->assertEquals('2013-04-07', $firstStatus->getDate()->format('Y-m-d'));
+    }
+
+    public function getMetadataTest()
+    {
+        return array(
+            array(
+                new TwitterList('futurecat', 'sensio', $this->getClient()),
+                'https://api.twitter.com/1/lists/statuses.json?owner_screen_name=futurecat&slug=sensio&include_entities=true',
+                'https://twitter.com/futurecat/sensio',
+                'https://twitter.com/futurecat/sensio',
+            ),
+        );
+    }
+}
