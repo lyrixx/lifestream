16fc69f0db116ee4ba128c36ccd0ab24f2b8e92f
Grégoire Pineau <lyrixx@lyrixx.info>
2013-04-16 19:06:47 +0200

    TwitterList use AdvancedStatus

--- Lifestream/Service/TwitterList.php
+++ Lifestream/Service/TwitterList.php
@@ -10,8 +10,11 @@ use Guzzle\Http\Client;
 class TwitterList extends AbstractFeed
 {
     const FEED_URL    = 'https://api.twitter.com/1/lists/statuses.json?owner_screen_name=%s&slug=%s&include_entities=true';
-    const PROFILE_URL = 'https://twitter.com/%s/%s';
+    const PROFILE_URL = 'https://twitter.com/%s';
+    const LIST_URL = 'https://twitter.com/%s/%s';
     const TWEET_URL   = 'https://twitter.com/%s/statuses/%s';
+
+    protected $statusClassname = 'Lyrixx\Lifestream\Status\AdvancedStatus';
 
     /**
      * Constructor
@@ -23,7 +26,7 @@ class TwitterList extends AbstractFeed
     public function __construct($username, $list, Client $client = null)
     {
         $feedUrl = sprintf(self::FEED_URL, $username, $list);
-        $profileUrl = sprintf(self::PROFILE_URL, $username, $list);
+        $profileUrl = sprintf(self::LIST_URL, $username, $list);
 
         parent::__construct($feedUrl, $profileUrl, $client);
     }
@@ -46,10 +49,14 @@ class TwitterList extends AbstractFeed
      */
     protected function formatDatas($datas)
     {
-        return array_replace($datas, array(
+        return array(
             'text' => $datas['text'],
             'url'  => sprintf(static::TWEET_URL, $datas['user']['screen_name'], $datas['id_str']),
             'date' => new \Datetime($datas['created_at']),
-        ));
+            'username' => $datas['user']['screen_name'],
+            'fullname' => $datas['user']['name'],
+            'pictureUrl' => $datas['user']['profile_image_url'],
+            'profileUrl' => sprintf(static::PROFILE_URL, $datas['user']['screen_name']),
+        );
     }
 }
--- Lifestream/Tests/Service/TwitterListTest.php
+++ Lifestream/Tests/Service/TwitterListTest.php
@@ -15,9 +15,14 @@ class TwitterListTest extends AbstractTest
         $this->assertCount(12, $statuses);
 
         $firstStatus = $statuses[0];
+        $this->assertInstanceOf('Lyrixx\Lifestream\Status\AdvancedStatus', $firstStatus);
         $this->assertEquals('@lyrixx un autre jour ;)', $firstStatus->getText());
         $this->assertEquals('https://twitter.com/futurecat/statuses/320926532933672961', $firstStatus->getUrl());
         $this->assertEquals('2013-04-07', $firstStatus->getDate()->format('Y-m-d'));
+        $this->assertSame('futurecat', $firstStatus->getUsername());
+        $this->assertSame('Marc', $firstStatus->getFullname());
+        $this->assertSame('http://a0.twimg.com/profile_images/1153760106/Photo_du_62709773-10-___17.25_normal.jpg', $firstStatus->getPictureUrl());
+        $this->assertSame('https://twitter.com/futurecat', $firstStatus->getProfileUrl());
     }
 
     public function getMetadataTest()
